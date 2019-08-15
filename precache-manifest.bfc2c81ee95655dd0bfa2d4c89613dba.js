e6c69f9462140192556da36829385dc1d22e8950
bdsibert <bds_20_08@hotmail.com>
2019-08-15 01:00:24 -0400

    Updates

--- precache-manifest.bfc2c81ee95655dd0bfa2d4c89613dba.js
+++ precache-manifest.bfc2c81ee95655dd0bfa2d4c89613dba.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "41a24c86d56dd773669f0ed3f7e42b1e",
+    "revision": "80a18d4adc2a61f224ff44229565f7d8",
     "url": "/baseball-react-app/index.html"
   },
   {
-    "revision": "3593bbb776e7b503b8fc",
+    "revision": "c68e35eb27baf0487cd7",
     "url": "/baseball-react-app/static/css/main.29e4e5ac.chunk.css"
   },
   {
-    "revision": "61e61911de78e3648cec",
-    "url": "/baseball-react-app/static/js/2.dd93a481.chunk.js"
+    "revision": "a2e049620c3683f7e034",
+    "url": "/baseball-react-app/static/js/2.2e43b78a.chunk.js"
   },
   {
-    "revision": "3593bbb776e7b503b8fc",
-    "url": "/baseball-react-app/static/js/main.54cc4f77.chunk.js"
+    "revision": "c68e35eb27baf0487cd7",
+    "url": "/baseball-react-app/static/js/main.701710c8.chunk.js"
   },
   {
     "revision": "d3e39f9a1de16627abd1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/baseball-react-app/precache-manifest.b10b2963975c738ee887bc3c36e684a4.js"
+  "/baseball-react-app/precache-manifest.bfc2c81ee95655dd0bfa2d4c89613dba.js"
 );
 
 self.addEventListener('message', (event) => {
