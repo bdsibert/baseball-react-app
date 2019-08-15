04bc4042ccf123d51464d8a8f48fb3216e8441b3
bdsibert <bds_20_08@hotmail.com>
2019-08-15 00:35:29 -0400

    Updates

---
+++ precache-manifest.8ed53c780bae60b889f344fd616fcb59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e97e65f92861a205ac0fbb52d3540df2",
+    "url": "/baseball-react-app/index.html"
+  },
+  {
+    "revision": "848498377c2909a9dc14",
+    "url": "/baseball-react-app/static/css/main.29e4e5ac.chunk.css"
+  },
+  {
+    "revision": "61e61911de78e3648cec",
+    "url": "/baseball-react-app/static/js/2.dd93a481.chunk.js"
+  },
+  {
+    "revision": "848498377c2909a9dc14",
+    "url": "/baseball-react-app/static/js/main.1a72990f.chunk.js"
+  },
+  {
+    "revision": "d3e39f9a1de16627abd1",
+    "url": "/baseball-react-app/static/js/runtime~main.fdea3757.js"
+  }
+]);
